87cb28019aeb5e943fa0d482456bde58f46558c4
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-10-09 18:25:19 -0400

    Now tracking last empty event instead of last occupied/empty

--- managers/sensor_manager.py
+++ managers/sensor_manager.py
@@ -11,6 +11,8 @@ from utils.sensor_event import OccupiedEvent, SensorEvent, SensorState
 class SensorManager:
     last_sensor_event: SensorEvent = None
 
+    last_empty_event: SensorEvent = None
+
     def __init__(self, SENSOR_PIN: int, zone: str, q: asyncio.Queue) -> None:
         self.SENSOR_PIN = SENSOR_PIN
         GPIO.setmode(GPIO.BCM)
@@ -20,6 +22,7 @@ class SensorManager:
         self.event_queue = q
         self.sensor_state: SensorState = SensorState.EMPTY
         SensorManager.last_sensor_event = SensorEvent(self.zone, self.sensor_state)
+        SensorManager.last_empty_event = SensorEvent(self.zone, self.sensor_state)
     
     async def loop(self) -> None:
         while True:
@@ -37,9 +40,11 @@ class SensorManager:
 
             occupied_event = OccupiedEvent(self.zone, self.last_sensor_event.rpi_time, datetime.now(timezone.utc).timestamp())
             await self.event_queue.put(occupied_event)
-
+        elif SensorState(current_state) == SensorState.EMPTY:
+            SensorManager.last_empty_event = SensorEvent(self.zone, current_state)
         self.sensor_state = current_state
         SensorManager.last_sensor_event = SensorEvent(self.zone, current_state)
+        
 
 
         #if current_state != self.sensor_state:
