f4ed67c66bfb655aa433e58e6b1466184168bfbc
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-10-10 17:50:57 -0400

    try

--- managers/led_manager.py
+++ managers/led_manager.py
@@ -41,7 +41,7 @@ class LedManager:
 
             if pixelsToHighlight > pixelsFloored:
                 self.leds.setPixel(self.index, pixelsFloored + 1,
-                                   Color(0, int(255 * pixelsToHighlight % 1), 0))
+                                   Color(0, int(255 * (pixelsToHighlight % 1)), 0))
             print(pixelsToHighlight)
             self.leds.show()
             pass
