6ee5d0acf91ec6dd1c4c6a40152d9fa5ebd72bad
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-10-17 13:18:03 -0400

    asd

--- utils/utils.py
+++ utils/utils.py
@@ -50,7 +50,7 @@ class PixelStrip:
         self.indicatorCount = indicatorCount
         self.indicatorNumPixels = int(self.ledsCount / self.indicatorCount)
         self.strip = Adafruit_NeoPixel(
-            self.ledsCount, gpio, hz, dma, invert, brightness, channel, ws.SK6812_SHIFT_WMASK)
+            self.ledsCount, gpio, hz, dma, invert, brightness, channel, ws.WS2811_STRIP_RGB)
         self.strip.begin()
 
     def show(self):
