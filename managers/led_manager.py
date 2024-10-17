fb26c958153b5894265e2bb20f0c1bb174fbc3ca
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-10-17 12:33:53 -0400

    asd

--- managers/led_manager.py
+++ managers/led_manager.py
@@ -25,10 +25,9 @@ class LedManager:
         event = self.sensor.last_sensor_event
         event_duration = datetime.now(timezone.utc).timestamp(
         ) - self.sensor.last_empty_event.rpi_time
-        print("Current Time: " + "{:.2f}".format(event_duration))
+        print("Event Duration: " + "{:.2f}".format(event_duration))
         stage_index = self.get_led_stage_index(event_duration)
         stage = Config.get()["leds"]["stages"][stage_index]
-        print("Current Stage: " + str(stage_index))
 
         if stage_index != -1:
             time_into_stage = event_duration - \
@@ -64,68 +63,6 @@ class LedManager:
                 output = Color(clamp(primary.r + secondary.r, 0, 255), clamp(primary.g +
                                secondary.g, 0, 255), clamp(primary.b + secondary.b, 0, 255))
 
-                # col = Color(255 - t, 255 - t, t)
-                self.leds.fill(self.index, output)
-            else:
-                self.leds.fill(self.index, hex_to_rgb(stage["color"]))
-
-        return
-        event = self.sensor.last_sensor_event
-        if event.state == SensorState.EMPTY.value:
-            self.leds.fill(self.index, Color(0, 0, 0))
-            return
-
-        event_duration = datetime.now(
-            timezone.utc).timestamp() - self.sensor.last_empty_event.rpi_time
-
-        last_stage = Config.get()["leds"]["stages"][-1]
-        if event_duration >= last_stage["duration"]:
-            stage = last_stage
-        else:
-            stage_index = self.get_led_stage_index(event_duration)
-            stage = Config.get()["leds"]["stages"][stage_index]
-
-        timer_duration = last_stage["duration"]
-
-        print("==========")
-        print("Current Time: " + str(event_duration))
-        print("End Stage Time: " + str(stage["duration"]))
-        print("End Time: " + str(timer_duration))
-        print()
-        # await asyncio.sleep(2)
-        if event_duration <= timer_duration:
-            # figure out how far into timer we are
-            val = event_duration / timer_duration
-            # convert that to numpixels
-            pixelsToHighlight = val * self.leds.indicatorNumPixels
-            pixelsFloored = int(pixelsToHighlight)
-            for i in range(pixelsFloored):
-                self.leds.setPixel(self.index, i, hex_to_rgb(stage["color"]))
-
-            if pixelsToHighlight > pixelsFloored:
-                self.leds.setPixel(self.index, pixelsToHighlight,
-                                   hex_to_rgb(stage["color"], pixelsToHighlight % 1))
-
-            self.leds.show()
-        else:
-            if Config.get()["leds"]["flashing"]["shouldFlash"]:
-                # get time sine wave
-                # map from -1 to 1
-                v = math.sin(datetime.now(timezone.utc).timestamp(
-                ) * Config.get()["leds"]["flashing"]["flashFrequency"])
-
-                t = inv_lerp(1, -1, v)
-                y = inv_lerp(1, -1, -v)
-
-                primary = hex_to_rgb(
-                    Config.get()["leds"]["flashing"]["primaryColor"], t)
-                secondary = hex_to_rgb(
-                    Config.get()["leds"]["flashing"]["secondaryColor"], y)
-
-                output = Color(clamp(primary.r + secondary.r, 0, 255), clamp(primary.g +
-                               secondary.g, 0, 255), clamp(primary.b + secondary.b, 0, 255))
-
-                # col = Color(255 - t, 255 - t, t)
                 self.leds.fill(self.index, output)
             else:
                 self.leds.fill(self.index, hex_to_rgb(stage["color"]))
