433234c450699cfdc4ccf028864f19d55a58631f
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-10-29 18:44:56 -0400

    asd

--- managers/led_manager.py
+++ managers/led_manager.py
@@ -22,6 +22,9 @@ class LedManager:
             await asyncio.sleep(.01)
 
     async def process_event(self) -> None:
+        for i in range(self.leds.indicatorNumPixels):
+            self.leds.setPixel(self.index, i, Color(255, 255, 255))
+        return
         event = self.sensor.last_empty_event
         event_duration = datetime.now(timezone.utc).timestamp(
         ) - event.rpi_time.timestamp()
