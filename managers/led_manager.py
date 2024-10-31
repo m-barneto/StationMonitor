1de86de71482e1d9c297d2a41d6f349635eee37b
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-10-31 13:26:04 -0400

    asd

--- managers/led_manager.py
+++ managers/led_manager.py
@@ -30,7 +30,7 @@ class LedManager:
             1,
             1)
         col = Color(int(colt[0] * 255), int(colt[1] * 255), int(colt[2] * 255))
-        print(col)
+        print(int(colt[0] * 255), int(colt[1] * 255), int(colt[2] * 255))
         self.leds.fill(self.index, col)
 
         self.leds.show()
