7aaada6f05006664bb2bff1f73d0c90204883391
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-12-04 08:49:07 -0500

    Commenting out main area.

--- client.py
+++ client.py
@@ -15,27 +15,35 @@ from utils.utils import PixelStrip
 from rpi_ws281x import Color
 
 leds = PixelStrip(Config.get()["leds"]["numLeds"],
-                 Config.get()["leds"]["numIndicators"],
-                 Config.get()["leds"]["gpioPin"],
-                 Config.get()["leds"]["brightness"])
-
-# webserver :(
+                  Config.get()["leds"]["numIndicators"],
+                  Config.get()["leds"]["gpioPin"],
+                  Config.get()["leds"]["brightness"])
 
 
 try:
+    # Initialize our queues
     event_queue = asyncio.Queue()
     alarm_queue = asyncio.Queue()
 
+    # Create main event loop
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
+    # Manager that sends out requests containing event data
     event_manager = EventManager(event_queue)
     loop.create_task(event_manager.loop())
+
+    # Sends out requests for alarm events when duration is exceeded
     loop.create_task(AlarmManager(alarm_queue).loop())
+
+    # Syncs config from lenovo
     loop.create_task(ConfigManager().loop())
+
+    # Handles sleep state when site is closed
     sleep_manager = SleepManager()
     loop.create_task(sleep_manager.loop())
 
+    # List to store sensor managers in
     sensors = []
 
     # Initialize sensors from config entries
@@ -48,12 +56,19 @@ try:
             alarm_queue
         )
         sensors.append(s)
+
+        # Setup led manager for this sensor
         l = LedManager(s, leds, sensor["indicatorIndex"])
+
+        # Initialize our event loops for the sensor managers
         loop.create_task(s.loop())
         loop.create_task(l.loop())
 
+    # Web server that displays current status of sensors to web
     server = ServerManager(sensors, event_manager, sleep_manager)
     loop.create_task(server.loop())
+
+    # Start our event loop
     loop.run_forever()
 finally:
     # Cleanup GPIO pin
