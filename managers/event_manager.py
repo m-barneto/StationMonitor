892a07f619d9a486204551cbc4b510fbe89982e6
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-01-21 09:33:48 -0500

    Remove debug print

--- managers/event_manager.py
+++ managers/event_manager.py
@@ -29,7 +29,7 @@ class EventManager:
             try:
                 # Sends the request while still allowing other loops to continue running
                 res = await loop.run_in_executor(None, requests.post, Config.get()["proxyEventRoute"], None, json.loads(json.dumps(self.current_event.__dict__, default=str)))
-                print(res.status_code)
+                
                 # This is our rate limiting sleep
                 await asyncio.sleep(float(1 / int(Config.get()["eventSendRate"])))
                 # Break out of loop to allow us to process the next event in the queue
