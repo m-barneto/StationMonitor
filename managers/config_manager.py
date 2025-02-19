502b92e53472bbfef058de3cbbab95f8c124ae1e
t-a11en <thomasalleninbox@gmail.com>
2025-02-19 16:29:48 -0500

    Local JSONC swap

--- managers/config_manager.py
+++ managers/config_manager.py
@@ -20,12 +20,17 @@ class ConfigManager:
     @staticmethod
     async def update_config() -> None:
         try:
-            res = requests.get("http://192.168.17.202/config.jsonc")
-            remote_config = pyjson5.loads(res.text)
+            with open("config.jsonc", "r", encoding="utf-8") as file:
+                jsonc_content = file.read()
+            remote_config = pyjson5.loads(jsonc_content)
             orig = deepcopy(Config.conf)
-
             Config.conf = remote_config
             Config.conf["sensors"] = orig["sensors"]
-        except requests.exceptions.ConnectionError as e:
-            # sleep for a bit to avoid spamming a downed proxy
+        except FileNotFoundError:
+            print("Error: 'config.jsonc' file is not found.")
+            # sleep for a bit to avoid spamming when local config file isn't found
+            await asyncio.sleep(60)
+        except Exception as e:
+            print(f"An error occured when loading local jsonc config file: {e}")
+            # sleep for a bit to avoid spamming if another error does occur when loading config jsonc file
             await asyncio.sleep(60)
