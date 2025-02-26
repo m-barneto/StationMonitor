2b2859890c0693ed91f911b186c69a34b0841be4
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-02-26 10:50:18 -0500

    Added basic auth to health manager

--- managers/health_manager.py
+++ managers/health_manager.py
@@ -1,6 +1,6 @@
 import asyncio
+from functools import partial
 import json
-import logging
 
 import requests
 
@@ -17,12 +17,9 @@ class HealthManager:
         while True:
             try:
                 # Sends the request while still allowing other loops to continue running
-                res = await loop.run_in_executor(None, requests.post, Config.get()["proxyStatusUpdateRoute"], None, json.loads(json.dumps(ServerManager.status_dict(), default=str)))
-                
+                res = await loop.run_in_executor(None, partial(requests.post, url=Config.get()["proxyEventRoute"], json=json.loads(json.dumps(ServerManager.status_dict(), default=str)), auth=("automsvc", "speed0Meter!")))
                 # This is our rate limiting sleep
-                await asyncio.sleep(int(Config.get()["updateHealthStatusInterval"]) * 60)
-                # Break out of loop to allow us to process the next event in the queue
-                break
+                await asyncio.sleep(float(Config.get()["updateHealthStatusInterval"]) * 60)
             except requests.exceptions.ConnectionError as e:
                 # sleep for a bit to avoid spamming a downed proxy
                 await asyncio.sleep(60)
