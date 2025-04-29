b5aece705af485c8cfcf85423415898fc7574fea
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-04-29 13:29:45 -0400

    asd

--- sensors/distance_sensor.py
+++ sensors/distance_sensor.py
@@ -94,5 +94,5 @@ class DistanceSensor(Sensor):
                     self.current_distance = dis1
                     self.state = SensorState.OCCUPIED if self.is_occupied() else SensorState.EMPTY
                     print(f"Dynamic Distance: {dis1} mm")
-                    ser.flush()
+                    ser.flushInput()
                     await asyncio.sleep(.25)
