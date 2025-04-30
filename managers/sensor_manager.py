37898891e04fdaf7b9e935dd54f1bdb9f5a9c377
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-04-30 11:24:31 -0400

    asd

--- managers/sensor_manager.py
+++ managers/sensor_manager.py
@@ -79,7 +79,7 @@ class SensorManager:
         event_state: EventState
 
         # Check if the sensor state has changed
-        if sensor_state != zone_ctx.previous_sensor_state:
+        if sensor_state != zone_ctx.previous_sensor_state or sensor_state != SensorState.EMPTY:
             if sensor_state == SensorState.EMPTY:
                 if zone_ctx.previous_event_state == EventState.OCCUPIED_STARTED:
                     # If the previous state was occupied and now it's empty, set to EMPTY
