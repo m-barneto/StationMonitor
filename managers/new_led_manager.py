c7c502e18b54485465aa85d70ae907cc84e7ddcd
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-06-11 08:47:02 -0400

    asd

--- managers/new_led_manager.py
+++ managers/new_led_manager.py
@@ -115,7 +115,7 @@ class LedManager:
         current_time = datetime.now(timezone.utc)
         # get the "stage" of the sensor
         ctx: SensorContext = self.sensor_manager.get_sensor_ctx(sensor)
-        duration = (current_time - ctx.occupied_start_time).total_seconds()
+        duration = (current_time - ctx.occupied_start_time).total_seconds() * 10
         state: EventState = ctx.current_event_state
 
         current_time = time()
