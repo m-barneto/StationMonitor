65d5029047505a80fd416765972dfd30b269f2dc
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-06-19 08:41:34 -0400

    asd

--- utils/logger.py
+++ utils/logger.py
@@ -8,6 +8,8 @@ logger.setLevel(logging.INFO)
 handler = RotatingFileHandler(
     "logs/station_monitor.log", maxBytes=1024 * 1024, backupCount=5
 )
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
 handler.setLevel(logging.INFO)
 
 logger.addHandler(handler)
