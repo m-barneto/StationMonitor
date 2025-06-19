45fc432cf0812c9c673b68bdbf3c80599b3037db
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-06-19 08:32:32 -0400

    asd

--- managers/event_manager.py
+++ managers/event_manager.py
@@ -30,7 +30,8 @@ class EventManager:
             try:
                 # Sends the request while still allowing other loops to continue running
                 res = await loop.run_in_executor(None, partial(requests.post, url=Config.get().proxyEventRoute, json=json.loads(json.dumps(self.current_event.to_dict(), default=str)), auth=("automsvc", "speed0Meter!")))
-                
+                print(f"Event sent: {self.current_event.to_dict()}")
+                print(f"Response: {res.status_code} - {res.text}")
                 # This is our rate limiting sleep
                 await asyncio.sleep(float(1 / int(Config.get().eventSendRate)))
                 # Break out of loop to allow us to process the next event in the queue
