9ff5bf5d307eb92a17afb168a5f8e35806c0a403
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-07-02 15:21:35 -0400

    Remove unused code

--- client.py
+++ client.py
@@ -50,16 +50,8 @@ try:
         #                  ref_sensor["indicatorPin"],
         #                  ref_sensor["pwmChannel"],
         #                  Config.get()["leds"]["brightness"])
-        #
-        #
-#
-        #s = SensorManager(
-        #    ref_sensor["gpioPin"],
-        #    ref_sensor["zone"],
-        #    ref_sensor["alarmDuration"],
-        #    event_queue,
-        #    alarm_queue
-        #)
+        
+        # Initialize our reflective sensor with the config and event queue
         s = ReflectiveSensor(
             ref_sensor,
             event_queue
