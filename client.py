175b451169885c552d0dd9050bc3872d86118d99
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-09-09 11:05:16 -0400

    Removing leds

--- client.py
+++ client.py
@@ -6,7 +6,6 @@ from managers.config_manager import ConfigManager
 from managers.event_manager import EventManager
 from managers.health_manager import HealthManager
 from managers.sensor_manager import SensorManager
-from managers.led_manager import LedManager
 
 from managers.server_manager import ServerManager
 from managers.sleep_manager import SleepManager
@@ -14,88 +13,73 @@ from sensors.distance_sensor import DistanceSensor
 from sensors.long_distance_sensor import LongDistanceSensor
 from sensors.sensor import Sensor
 from utils.config import Config
-from utils.utils import PixelStrip
 from utils.logger import logger
 
-try:
-    # Initialize our queues
-    event_queue = asyncio.Queue()
+# Initialize our queues
+event_queue = asyncio.Queue()
 
-    # Create main event loop
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+# Create main event loop
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 
-    # Manager that sends out requests containing event data
-    event_manager = EventManager(event_queue)
-    loop.create_task(event_manager.loop())
+# Manager that sends out requests containing event data
+event_manager = EventManager(event_queue)
+loop.create_task(event_manager.loop())
 
-    # Syncs config from lenovo
-    #loop.create_task(ConfigManager().loop())
+# Syncs config from lenovo
+#loop.create_task(ConfigManager().loop())
 
-    # Handles sleep state when site is closed
-    sleep_manager = SleepManager()
-    loop.create_task(sleep_manager.loop())
+# Handles sleep state when site is closed
+sleep_manager = SleepManager()
+loop.create_task(sleep_manager.loop())
 
-    sensors: List[Sensor] = []
+sensors: List[Sensor] = []
 
-    distance_sensors = []
+distance_sensors = []
 
-    for dist_sensor in Config.get().distanceSensors:
-        s = DistanceSensor(
-            dist_sensor,
-            event_queue
-        )
-        
-        distance_sensors.append(s)
-        sensors.append(s)
+for dist_sensor in Config.get().distanceSensors:
+    s = DistanceSensor(
+        dist_sensor,
+        event_queue
+    )
+    
+    distance_sensors.append(s)
+    sensors.append(s)
 
-        loop.create_task(s.loop())
+    loop.create_task(s.loop())
 
-    long_distance_sensors = []
+long_distance_sensors = []
 
-    for long_dist_sensor in Config.get().longDistanceSensors:
-        s = LongDistanceSensor(
-            long_dist_sensor,
-            event_queue
-        )
-        
-        long_distance_sensors.append(s)
-        sensors.append(s)
+for long_dist_sensor in Config.get().longDistanceSensors:
+    s = LongDistanceSensor(
+        long_dist_sensor,
+        event_queue
+    )
+    
+    long_distance_sensors.append(s)
+    sensors.append(s)
 
-        loop.create_task(s.loop())
+    loop.create_task(s.loop())
 
-    sensor_manager = SensorManager(sensors, event_queue)
-    loop.create_task(sensor_manager.loop())
+sensor_manager = SensorManager(sensors, event_queue)
+loop.create_task(sensor_manager.loop())
 
-    for sensor in long_distance_sensors:
-        # Initialize our led strip
-        leds = PixelStrip(Config.get().leds.numLeds,
-                          sensor.indicator_pin,
-                          sensor.pwm_channel,
-                          Config.get().leds.brightness)
-        # Setup led manager for this sensor
-        led_manager = LedManager(sensor.zone, sensor_manager, leds)
-        loop.create_task(led_manager.loop())
+print("Initialization complete, starting main loop")
+#led_manager = LedManager(reflective_sensors, sensor_manager)
+#loop.create_task(led_manager.loop())
 
-    print("Initialization complete, starting main loop")
-    #led_manager = LedManager(reflective_sensors, sensor_manager)
-    #loop.create_task(led_manager.loop())
+# Sends out requests for alarm events when duration is exceeded
+loop.create_task(AlarmManager(sensor_manager, event_queue).loop())
 
-    # Sends out requests for alarm events when duration is exceeded
-    loop.create_task(AlarmManager(sensor_manager, event_queue).loop())
+print("Starting web server on port 80")
+# Web server that displays current status of sensors to web
+server = ServerManager(distance_sensors, long_distance_sensors, sensor_manager, event_manager, sleep_manager)
+loop.create_task(server.loop())
 
-    print("Starting web server on port 80")
-    # Web server that displays current status of sensors to web
-    server = ServerManager(reflective_sensors, distance_sensors, long_distance_sensors, sensor_manager, event_manager, sleep_manager)
-    loop.create_task(server.loop())
+print("Finished starting web server")
 
-    print("Finished starting web server")
+status_updater = HealthManager()
+loop.create_task(status_updater.loop())
 
-    status_updater = HealthManager()
-    loop.create_task(status_updater.loop())
-
-    # Start our event loop
-    loop.run_forever()
-finally:
-    # Cleanup GPIO pin
-    GPIO.cleanup()
+# Start our event loop
+loop.run_forever()
