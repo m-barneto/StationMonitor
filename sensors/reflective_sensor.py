e78d20e197e050893a2fef673cf9aba67fd882c6
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-10-09 09:46:28 -0400

    asd

--- sensors/reflective_sensor.py
+++ sensors/reflective_sensor.py
@@ -1,23 +1,13 @@
 import asyncio
 from dataclasses import dataclass
 from sensors.sensor import Sensor, SensorState
-import RPi.GPIO as GPIO
 
 from utils.config import Config, ReflectiveSensorConfig  # type: ignore
 
 class ReflectiveSensor(Sensor):
     def __init__(self, config: ReflectiveSensorConfig, event_queue: asyncio.Queue):
         """Initialize the reflective sensor with the given configuration."""
-        Sensor.__init__(self, config.zone, event_queue)
-
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(config.gpioPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        self.gpio_pin = config.gpioPin
+        pass
 
     async def loop(self) -> None:
-        while True:
-            # Get current state of sensor
-            self.current_reading = GPIO.input(self.gpio_pin)
-            self.state = SensorState.OCCUPIED if self.current_reading else SensorState.EMPTY
-            await asyncio.sleep(float(1 / Config.get().sensorPollRate))
+        pass
