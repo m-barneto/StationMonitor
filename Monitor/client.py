f5acf4c55f5615dac0d3ffd41659315f516ea466
m-barneto <4347791+m-barneto@users.noreply.github.com>
2025-10-22 17:22:37 -0400

    asd

--- Monitor/client.py
+++ Monitor/client.py
@@ -56,7 +56,7 @@ loop.create_task(sensor_manager.loop())
 loop.create_task(AlarmManager(sensor_manager, event_queue).loop())
 
 # Handle the timer management
-loop.create_tast(TimerManager(sensors).loop())
+loop.create_task(TimerManager(sensors).loop())
 
 # Web server that displays current status of sensors to web
 server = ServerManager(long_distance_sensors, sensor_manager, event_manager, sleep_manager)
