0616e341943066a7f55b3972af0f38b20b4a7794
m-barneto <4347791+m-barneto@users.noreply.github.com>
2026-02-12 11:30:50 -0500

    asd

--- Interface/src/components/Speedometer.tsx
+++ Interface/src/components/Speedometer.tsx
@@ -37,6 +37,7 @@ export default function Speedometer() {
                 display: "flex",
                 justifyContent: "center",
                 gap: "5rem",
+                height: "30%"
             }}>
             <div>
                 <h2
