f999daef53f63ccdcf8b6c7f8da5a480e622f38c
m-barneto <4347791+m-barneto@users.noreply.github.com>
2026-02-17 14:10:59 -0500

    asd

--- Monitor/managers/cache_manager.py
+++ Monitor/managers/cache_manager.py
@@ -32,8 +32,7 @@ class CacheManager:
     @staticmethod
     def prune_events():
         # loop through the event cache backwards and remove events that are older than x hours.
-        #TODO this should be hours not minutes
-        cutoff = datetime.now(timezone.utc) - timedelta(minutes=Config.get().standalone.pruneHours)
+        cutoff = datetime.now(timezone.utc) - timedelta(hours=Config.get().standalone.pruneHours)
         while CacheManager.event_cache:
             start_time = datetime.fromisoformat(CacheManager.event_cache[0]["body"]["startTime"])
             if start_time >= cutoff:
