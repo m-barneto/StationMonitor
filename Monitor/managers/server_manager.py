568cf8dec87442524bc629055b6ddbc341c5b31d
m-barneto <4347791+m-barneto@users.noreply.github.com>
2026-02-19 16:46:08 -0500

    asd

--- Monitor/managers/server_manager.py
+++ Monitor/managers/server_manager.py
@@ -172,6 +172,7 @@ class ServerManager:
             data = await request.json()
             dt = datetime.fromisoformat(data["time"])
             formatted_time = dt.strftime("'%Y-%m-%d %H:%M:%S'")
+            print(formatted_time)
             result = subprocess.run(["sudo", "timedatectl", "set-time", formatted_time], check=True, capture_output=True, text=True)
             print(result.returncode)
             print(result.stdout)
