851f8223eb16a6ccb91ddc699c1a34f933c417b6
m-barneto <4347791+m-barneto@users.noreply.github.com>
2026-02-19 16:52:46 -0500

    asd

--- Monitor/managers/server_manager.py
+++ Monitor/managers/server_manager.py
@@ -175,12 +175,8 @@ class ServerManager:
                 time_string = time_string.replace("Z", "+00:00")
             dt = datetime.fromisoformat(time_string)
             formatted_time = dt.strftime("%Y-%m-%d %H:%M:%S")
-
             print(formatted_time)
             result = subprocess.run(["sudo", "timedatectl", "set-time", formatted_time], check=True, capture_output=True, text=True)
-            print(result.returncode)
-            print(result.stdout)
-            print(result.stderr)
             return web.Response(text="Synced time successfully.", status=200)
         except Exception as e:
             return web.Response(text=f"Error syncing time: {str(e)}", status=500)
