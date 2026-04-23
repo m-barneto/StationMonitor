045ea1805501bdf95680fe60cb9728d88e0b9d6a
m-barneto <4347791+m-barneto@users.noreply.github.com>
2026-04-23 09:55:19 -0400

    asd

--- Monitor/utils/timer.py
+++ Monitor/utils/timer.py
@@ -13,11 +13,13 @@ class Timer:
         self.has_started = False
 
     async def start(self):
+        print(f"Starting timer for zone {self.zone}")
         self.start_io.write(True)
         await asyncio.sleep(0.25)
         self.start_io.write(False)
 
     async def reset(self):
+        print(f"Resetting timer for zone {self.zone}")
         self.reset_io.write(True)
         await asyncio.sleep(0.25)
         self.reset_io.write(False)
